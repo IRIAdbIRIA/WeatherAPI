6a3c51fe46e41922e9d57f7b7c6a029177c23a91
IRIAdbIRIA <71179574+IRIAdbIRIA@users.noreply.github.com>
2022-09-26 12:17:07 +0200

    Add files via upload

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,12 @@
-INSERT INTO city (city_name, lat, lon) VALUES
-                                      ('Kraków', 50.049683, 19.944544),
-                                      ('Gdańsk', 54.372158, 18.638306),
-                                      ('Warszawa', 52.237049, 21.017532),
-                                      ('Lublin', 51.246452, 22.568445),
-                                      ('Katowice', 50.270908, 19.039993),
-                                      ('Berlin', 52.520008, 13.404954),
-                                      ('Paryż', 48.856613, 2.352222),
-                                      ('London', 51.507351, -0.127758),
-                                      ('NowyYork', 40.712776, -74.005974),
-                                      ('Toruń', 53.013790, 18.598444);
+
+
+INSERT INTO city (city_name, lat, lon) VALUES ('Kraków', 50.049683, 19.944544);
+INSERT INTO city (city_name, lat, lon) VALUES ('Gdańsk', 54.372158, 18.638306);
+INSERT INTO city (city_name, lat, lon) VALUES ('Warszawa', 52.237049, 21.017532);
+INSERT INTO city (city_name, lat, lon) VALUES ('Lublin', 51.246452, 22.568445);
+INSERT INTO city (city_name, lat, lon) VALUES ('Katowice', 50.270908, 19.039993);
+INSERT INTO city (city_name, lat, lon) VALUES ('Berlin', 52.520008, 13.404954);
+INSERT INTO city (city_name, lat, lon) VALUES ('Paryż', 48.856613, 2.352222);
+INSERT INTO city (city_name, lat, lon) VALUES ('London', 51.507351, -0.127758);
+INSERT INTO city (city_name, lat, lon) VALUES ('Nowy York', 40.712776, -74.005974);
+INSERT INTO city (city_name, lat, lon) VALUES ('Toruń', 53.013790, 18.598444);
